5327f0e7baba2baab62927d313efc080f82b6008
srajfo <132372038+srajfo@users.noreply.github.com>
2025-05-12 12:32:37 +0200

    Add files via upload

---
+++ vjezba na satu/ispit_tcp_udp/serverIspit/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace serverIspit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_start_udp = new System.Windows.Forms.Button();
+            this.bt_start_tcp = new System.Windows.Forms.Button();
+            this.lB_udp = new System.Windows.Forms.ListBox();
+            this.lB_tcp = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bt_nmap_start = new System.Windows.Forms.Button();
+            this.lb_nmap_ispis = new System.Windows.Forms.ListBox();
+            this.tB_ip_start = new System.Windows.Forms.TextBox();
+            this.tB_ip_end = new System.Windows.Forms.TextBox();
+            this.tB_port_start = new System.Windows.Forms.TextBox();
+            this.tB_port_end = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tB_portovi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bt_start_udp
+            // 
+            this.bt_start_udp.Location = new System.Drawing.Point(12, 76);
+            this.bt_start_udp.Name = "bt_start_udp";
+            this.bt_start_udp.Size = new System.Drawing.Size(75, 23);
+            this.bt_start_udp.TabIndex = 0;
+            this.bt_start_udp.Text = "UDP";
+            this.bt_start_udp.UseVisualStyleBackColor = true;
+            this.bt_start_udp.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bt_start_tcp
+            // 
+            this.bt_start_tcp.Location = new System.Drawing.Point(252, 76);
+            this.bt_start_tcp.Name = "bt_start_tcp";
+            this.bt_start_tcp.Size = new System.Drawing.Size(75, 23);
+            this.bt_start_tcp.TabIndex = 1;
+            this.bt_start_tcp.Text = "TCP";
+            this.bt_start_tcp.UseVisualStyleBackColor = true;
+            this.bt_start_tcp.Click += new System.EventHandler(this.bt_start_tcp_Click);
+            // 
+            // lB_udp
+            // 
+            this.lB_udp.FormattingEnabled = true;
+            this.lB_udp.Location = new System.Drawing.Point(12, 105);
+            this.lB_udp.Name = "lB_udp";
+            this.lB_udp.Size = new System.Drawing.Size(161, 498);
+            this.lB_udp.TabIndex = 2;
+            // 
+            // lB_tcp
+            // 
+            this.lB_tcp.FormattingEnabled = true;
+            this.lB_tcp.Location = new System.Drawing.Point(252, 105);
+            this.lB_tcp.Name = "lB_tcp";
+            this.lB_tcp.Size = new System.Drawing.Size(161, 498);
+            this.lB_tcp.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // bt_nmap_start
+            // 
+            this.bt_nmap_start.Location = new System.Drawing.Point(487, 76);
+            this.bt_nmap_start.Name = "bt_nmap_start";
+            this.bt_nmap_start.Size = new System.Drawing.Size(75, 23);
+            this.bt_nmap_start.TabIndex = 5;
+            this.bt_nmap_start.Text = "nmap";
+            this.bt_nmap_start.UseVisualStyleBackColor = true;
+            this.bt_nmap_start.Click += new System.EventHandler(this.bt_nmap_start_Click);
+            // 
+            // lb_nmap_ispis
+            // 
+            this.lb_nmap_ispis.FormattingEnabled = true;
+            this.lb_nmap_ispis.Location = new System.Drawing.Point(487, 105);
+            this.lb_nmap_ispis.Name = "lb_nmap_ispis";
+            this.lb_nmap_ispis.Size = new System.Drawing.Size(252, 498);
+            this.lb_nmap_ispis.TabIndex = 6;
+            // 
+            // tB_ip_start
+            // 
+            this.tB_ip_start.Location = new System.Drawing.Point(766, 105);
+            this.tB_ip_start.Name = "tB_ip_start";
+            this.tB_ip_start.Size = new System.Drawing.Size(100, 20);
+            this.tB_ip_start.TabIndex = 7;
+            // 
+            // tB_ip_end
+            // 
+            this.tB_ip_end.Location = new System.Drawing.Point(930, 105);
+            this.tB_ip_end.Name = "tB_ip_end";
+            this.tB_ip_end.Size = new System.Drawing.Size(100, 20);
+            this.tB_ip_end.TabIndex = 8;
+            // 
+            // tB_port_start
+            // 
+            this.tB_port_start.Location = new System.Drawing.Point(766, 190);
+            this.tB_port_start.Name = "tB_port_start";
+            this.tB_port_start.Size = new System.Drawing.Size(100, 20);
+            this.tB_port_start.TabIndex = 9;
+            // 
+            // tB_port_end
+            // 
+            this.tB_port_end.Location = new System.Drawing.Point(930, 190);
+            this.tB_port_end.Name = "tB_port_end";
+            this.tB_port_end.Size = new System.Drawing.Size(100, 20);
+            this.tB_port_end.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(763, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "PORTS";
+            // 
+            // tB_portovi
+            // 
+            this.tB_portovi.Location = new System.Drawing.Point(766, 336);
+            this.tB_portovi.Multiline = true;
+            this.tB_portovi.Name = "tB_portovi";
+            this.tB_portovi.Size = new System.Drawing.Size(405, 267);
+            this.tB_portovi.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1351, 648);
+            this.Controls.Add(this.tB_portovi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tB_port_end);
+            this.Controls.Add(this.tB_port_start);
+            this.Controls.Add(this.tB_ip_end);
+            this.Controls.Add(this.tB_ip_start);
+            this.Controls.Add(this.lb_nmap_ispis);
+            this.Controls.Add(this.bt_nmap_start);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lB_tcp);
+            this.Controls.Add(this.lB_udp);
+            this.Controls.Add(this.bt_start_tcp);
+            this.Controls.Add(this.bt_start_udp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_start_udp;
+        private System.Windows.Forms.Button bt_start_tcp;
+        private System.Windows.Forms.ListBox lB_udp;
+        private System.Windows.Forms.ListBox lB_tcp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button bt_nmap_start;
+        private System.Windows.Forms.ListBox lb_nmap_ispis;
+        private System.Windows.Forms.TextBox tB_ip_start;
+        private System.Windows.Forms.TextBox tB_ip_end;
+        private System.Windows.Forms.TextBox tB_port_start;
+        private System.Windows.Forms.TextBox tB_port_end;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tB_portovi;
+    }
+}
+
---
+++ vjezba na satu/ispit_tcp_udp/serverIspit/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace serverIspit
+{
+    public partial class Form1 : Form
+    {
+        List<TcpClient> clients = new List<TcpClient>();
+        List<TcpListener> listeners = new List<TcpListener>();
+        bool isRunning = false;
+
+        bool serverIsRunning = false;
+        string localIP;
+
+        List<String> listOfClients = new List<string>();
+        List<String> clientIds = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            localIP= "127.0.0.1";
+            string[] portStrings = tB_portovi.Text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            Thread thdUDPServer = new Thread(new ThreadStart(serverThread));
+            thdUDPServer.Start();
+            serverIsRunning = true;
+            foreach (string portStr in portStrings)
+            {
+                lB_udp.Items.Add("Server started on " + localIP + ", using port " + portStr);
+            }
+        }
+        public void serverThread(IPAddress iPAddress)
+        {
+            string[] portStrings = tB_portovi.Text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string portStr in portStrings)
+            {
+                IPEndPoint localEndPoint = new IPEndPoint(iPAddress.Parse("127.0.0.1"), portStr);
+                UdpClient udpServer = new UdpClient(localEndPoint);
+                while (true)
+                {
+                    var remoteEP = new IPEndPoint(IPAddress.Any, portStr);
+                    try
+                    {
+                        var data = udpServer.Receive(ref remoteEP);  // Receive data from client
+                        string clientIP = remoteEP.Address.ToString();
+                        string stringReceived = Encoding.UTF8.GetString(data);
+
+                        // Add message to ListBox on the UI thread
+                        Invoke((MethodInvoker)(() => lB_udp.Items.Add("Received data from: " + clientIP)));
+                        Invoke((MethodInvoker)(() => lB_udp.Items.Add("Received: " + stringReceived)));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error receiving data: " + ex.Message);
+                    }
+                }
+            }
+        }
+        private void bt_nmap_start_Click(object sender, EventArgs e)
+        {
+            int portStart = int.Parse(tB_port_start.Text);
+            int portEnd = int.Parse(tB_port_end.Text);
+            string IPStart = tB_ip_start.Text;
+            string IPEnd = IPStart;
+
+            bt_nmap_start.Enabled = false;
+
+            lb_nmap_ispis.Items.Clear();
+
+            Task.Run(() => ScanPorts(IPStart, IPEnd, portStart, portEnd));
+
+            bt_nmap_start.Enabled = true;
+        }
+        private List<string> GetIPRange(string startIP, string endIP)
+        {
+            List<string> ipList = new List<string>();
+
+            try
+            {
+                var startAddr = IPAddress.Parse(startIP);
+                var endAddr = IPAddress.Parse(endIP);
+
+                byte[] startBytes = startAddr.GetAddressBytes();
+                byte[] endBytes = endAddr.GetAddressBytes();
+
+                if (startBytes.Take(3).SequenceEqual(endBytes.Take(3)))
+                {
+                    for (int i = startBytes[3]; i <= endBytes[3]; i++)
+                    {
+                        byte[] newIP = new byte[4];
+                        Array.Copy(startBytes, newIP, 3);
+                        newIP[3] = (byte)i;
+                        ipList.Add(new IPAddress(newIP).ToString());
+                    }
+                }
+                else
+                {
+                    Log2("Invalid IP range. The IPs must be in the same network.");
+                }
+            }
+            catch
+            {
+                Log2("Invalid IP format.");
+            }
+
+            return ipList;
+        }
+        private void ScanPorts(string startIP, string endIP, int startPort, int endPort)
+        {
+            var ipRange = GetIPRange(startIP, endIP);
+
+            foreach (var ip in ipRange)
+            {
+                for (int port = startPort; port <= endPort; port++)
+                {
+                    TcpClient tcpClient = new TcpClient();
+                    try
+                    {
+                        var result = tcpClient.BeginConnect(ip, port, null, null);
+                        bool succes = result.AsyncWaitHandle.WaitOne(300);
+                        if (succes)
+                            Log2($"Port {port} on IP {ip} is OPEN.");
+                        else
+                            Log2($"Port {port} on IP {ip} is CLOSED.");
+                        //lB_nmap_ispis.Items.Add($"Port {port} on IP {ip} is OPEN.");
+                    }
+                    catch (Exception ex)
+                    {
+                        //Log2($"Port {port} on IP {ip} is CLOSED.");
+                    }
+                    finally
+                    {
+                        tcpClient.Close();
+                    }
+                }
+            }
+        }
+        private void Log2(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => Log2(message)));
+                return;
+            }
+            lb_nmap_ispis.Items.Add(message);
+        }
+
+        private void bt_start_tcp_Click(object sender, EventArgs e)
+        {
+            if (isRunning) return;
+
+            string[] portStrings = tB_portovi.Text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string portStr in portStrings)
+            {
+                if (int.TryParse(portStr, out int port))
+                {
+                    TcpListener listener = new TcpListener(IPAddress.Loopback, port);
+                    listener.Start();
+                    listeners.Add(listener);
+
+                    Thread t = new Thread(() => AcceptClients(listener));
+                    t.IsBackground = true;
+                    t.Start();
+
+                    Log($"listening na portu: {port}");
+                }
+                else
+                {
+                    Log($"nevaljani port: {portStr}");
+                }
+            }
+
+            isRunning = true;
+            bt_start_tcp.Enabled = false;
+            bt_start_tcp.Text = "runa";
+            bt_start_udp.Enabled = false;
+            bt_start_udp.Text = "runa tcp";
+        }
+        private void AcceptClients(TcpListener listener)
+        {
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    lock (clients)
+                    {
+                        clients.Add(client);
+                    }
+                    Thread t = new Thread(() => HandleClient(client));
+                    t.IsBackground = true;
+                    t.Start();
+
+                    Log("klijent spojen");
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+        private void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            try
+            {
+                while (true)
+                {
+                    int read = stream.Read(buffer, 0, buffer.Length);
+                    if (read == 0) break;
+
+                    string message = Encoding.UTF8.GetString(buffer, 0, read);
+                    Log("dobivena poruka: " + message);
+                    Broadcast(message, client);
+                }
+            }
+            catch { }
+
+            lock (clients)
+            {
+                clients.Remove(client);
+            }
+
+            client.Close();
+            Log("Klijent odspojen");
+        }
+
+        private void Broadcast(string message, TcpClient sender)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            lock (clients)
+            {
+                foreach (var client in clients)
+                {
+                    if (client != sender)
+                    {
+                        try
+                        {
+                            client.GetStream().Write(data, 0, data.Length);
+                        }
+                        catch { }
+                    }
+                }
+            }
+        }
+
+        private void Log(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => Log(message)));
+                return;
+            }
+            lB_tcp.Items.Add(message);
+        }
+    }
+}
